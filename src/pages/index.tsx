a702d7bc78ca0764062e354c029b26a831d240c4
Anderson Marlon <anderson18.marlon@gmail.com>
2026-09-21 16:07:50 -0300

    Added: next-seo - 21-09 16:07

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
+import NextSeo from "@/components/NextSeo";
+
 export default function Home() {
     return (
         <>
-
+            <NextSeo
+                title="X1 | X2"
+                description="X3"
+                canonical="https://.vercel.app/"
+                openGraph={{
+                    url: "https://.vercel.app/",
+                    title: "X1 | X2",
+                    description:
+                        "X3",
+                    siteName: "X1 | X2",
+                    images: [
+                        {
+                            url: "/thumbnail.png",
+                            width: 1280,
+                            height: 720,
+                            alt: "X1 | X2",
+                            type: "image/png",
+                        },
+                    ],
+                }}
+                twitter={{
+                    handle: "@",
+                    site: "@yagasaki7k",
+                    cardType: "summary_large_image",
+                }}
+            />
         </>
     );
 }

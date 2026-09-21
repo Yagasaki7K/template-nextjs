b4030857d2ce8e2a701052367ab2b1240dc36ae2
Anderson Marlon <anderson18.marlon@gmail.com>
2026-09-21 16:11:05 -0300

    Update Open Graph image dimensions

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -50,8 +50,8 @@ export default class MyDocument extends Document {
                     />
                     <meta property="og:image" content="/thumbnail.png" />
 
-                    <meta property="og:image:width" content="460" />
-                    <meta property="og:image:height" content="460" />
+                    <meta property="og:image:width" content="1280" />
+                    <meta property="og:image:height" content="720" />
                     <meta property="og:image:alt" content="" />
                     <meta property="og:image:type" content="image/png" />
                     <meta property="og:site_name" content="" />

553bad62b82e25c57737d3c578f766ffb5ba0d96
Anderson Marlon <anderson18.marlon@gmail.com>
2026-09-22 22:57:55 -0300

    Convert next.config.ts to TypeScript with export
    Refactor next.config.ts to use TypeScript types and export syntax.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  compiler: {
-    styledComponents: true,
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    compiler: {
+        styledComponents: true
+    }
 };
 
-module.exports = nextConfig;
+export default nextConfig;
